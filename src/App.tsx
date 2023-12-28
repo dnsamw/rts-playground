36c2c604624a49c6a7e874b0152c20711bc099e0
Dileepa Sam <50067526+dnsamw@users.noreply.github.com>
2023-12-28 21:47:37 +0530

    12- Exercise- Updating State - part 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,24 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, isFixed: false, msg: "Bug 1" },
-    { id: 2, isFixed: false, msg: "Bug 2" },
-    { id: 3, isFixed: false, msg: "Bug 3" },
-  ]);
+  const [game, setgame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
 
-  const fix = (bugId: any) => {
-    const newBugs = bugs.map((bug) =>
-      bug.id === bugId ? { ...bug, isFixed: true } : bug
-    );
-    setBugs(newBugs); 
+  const handleClick = (name:string)=>{
+    const newGame = {...game,player:{...game.player,name:name}}
+    setgame(newGame)
   };
 
   return (
-    <>
-      <div>
-        <h1>Bugs List</h1>
-      </div>
-      <div>
-        {bugs.map((bug) => (
-          <div key={bug.id}>
-            <span>
-              {bug.msg} - {bug.isFixed ? "Fixed" : "New"}
-            </span>
-            <button onClick={() => fix(bug.id)}>Fix</button>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="flex flex-col gap-4 p-8">
+      <div className="text-4xl">Name : {game.player.name}</div>
+      <div>Player</div>
+      <button className="bg-sky-500 rounded-full px-4 py-2 w-40" onClick={()=>handleClick("Sangakkara")}>Change Name</button>
+    </div>
   );
 }
 
