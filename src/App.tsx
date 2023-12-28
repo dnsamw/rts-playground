524341285c214ef0e221e82e3af1203e57c5d352
Dileepa Sam <50067526+dnsamw@users.noreply.github.com>
2023-12-29 01:08:18 +0530

    All 5 exercies in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import Exercise1 from "./components/Exercise1";
+import Exercise2 from "./components/Exercise2";
+import Exercise3 from "./components/Exercise3";
+import Exercise4 from "./components/Exercise4";
 import ExpandableText from "./components/ExpandableText";
 
 function App() {
   return (
     <div className="flex flex-col">
+      <Exercise1 />
+      <Exercise2 />
+      <Exercise3 />
+      <Exercise4 />
       <ExpandableText maxChars={50}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
         accusantium officiis error enim possimus eum deleniti saepe natus in
